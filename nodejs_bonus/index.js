5257c77fac8432d12e9c0b1341c19d8e913de205
jrpcoder <josepereira92@gmail.com>
2019-07-12 00:16:33 +0100

    finish templating, routing images and reformulation

--- nodejs_bonus/index.js
+++ nodejs_bonus/index.js
@@ -10,13 +10,39 @@ const server = http.createServer((req, res) => {
     const pathName = url.parse(req.url, true).pathname;
     const id = url.parse(req.url, true).query.id;
 
+    // products overview
     if (pathName === '/laptops' || pathName === '/') {
         res.writeHead(200, { 'Content-type': 'text/html' });
-        res.end('this is the products page!');
-    } else if (pathName === '/laptop' && id < laptopData.length) {
+        
+        fs.readFile(`${__dirname}/templates/template_overview.html`, 'utf-8', (err, data) => {
+            let overviewOutput = data;
+
+            fs.readFile(`${__dirname}/templates/template_card.html`, 'utf-8', (err, data) => {
+            const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join('');
+            overviewOutput = overviewOutput.replace('{%CARDS%}', cardsOutput);
+            res.end(overviewOutput);
+            });
+        });
+    } 
+    //products details
+    else if (pathName === '/laptop' && id < laptopData.length) {
         res.writeHead(200, { 'Content-type': 'text/html' });
-        res.end(`this is the laptop page for laptop ${id}`);
-    } else {
+
+        fs.readFile(`${__dirname}/templates/template_laptop.html`, 'utf-8', (err, data) => {
+            const laptop = laptopData[id];
+            const output = replaceTemplate(data, laptop);
+            res.end(output);
+        });
+    }
+    // images
+    else if ((/\.(jpg|jpeg|png|gif)$/i).test(pathName)) {
+        fs.readFile(`${__dirname}/data/img${pathName}`, (err, data) => {
+            res.writeHead(200, { 'Content-type': 'image/jpg' });
+            res.end(data);
+        });
+    } 
+    // url not found
+    else {
         res.writeHead(404, { 'Content-type': 'text/html' });
         res.end('this url was not found on the server!');
     }
@@ -25,3 +51,16 @@ const server = http.createServer((req, res) => {
 server.listen(3000, '127.0.0.1', () => {
     console.log('Server is listening for requests now!');
 });
+
+function replaceTemplate(originalHtml, laptop) {
+    let output = originalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+    output = output.replace(/{%IMAGE%}/g, laptop.image);
+    output = output.replace(/{%PRICE%}/g, laptop.price);
+    output = output.replace(/{%SCREEN%}/g, laptop.screen);
+    output = output.replace(/{%CPU%}/g, laptop.cpu);
+    output = output.replace(/{%STORAGE%}/g, laptop.storage);
+    output = output.replace(/{%RAM%}/g, laptop.ram);
+    output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+    output = output.replace(/{%ID%}/g, laptop.id);
+    return output;
+};
